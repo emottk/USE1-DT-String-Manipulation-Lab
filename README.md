ae793c600b3584a965e0da04e8cc5ca75e2d6d00
emottk <emily.kowal@postlight.com>
2016-10-03 13:43:28 -0400

    README and Lab addition

---
+++ README.md
@@ -0,0 +1,15 @@
+#String Manipulation Practice
+
+Follow the directions in the comments.
+When you're done and want to check your answer, `cd` into this directory in your command line and type `Ruby lab.rb`    
+
+Change any answer that doesn't show up as expected.   
+
+Hint: We can bring two strings together by using the `+` sign.
+This is called concatenation.     
+For example --
+
+  print "I like green eggs and " + "ham!"
+    >> "I like green eggs and ham!"
+
+You'll have a chance to try this out in the lab!
---
+++ lab.rb
@@ -0,0 +1,29 @@
+
+#Your dog is using a new device that helps it speak to you.
+#Add a method to the end of this string to calm this dog down.
+print "HI HI FEED ME PLEASE"
+
+#Uh oh- looks like the translator might be broken.
+#Add a method to the end of this string to translate what your dog is saying.
+
+print "zlp oga setunim evif eta I hguoht neve yrgnuh os ylsuoires m'I"
+
+#Loud and all mixed up!
+#Add two methods to the end of this string to help you understand your dog better.
+print "!DOOF EM EVIG TSUJ ZLP TAHT EKIL EM TA GNIKOOL UOY ERA YHW"
+
+#Looks like your cat got hold of the translator.
+#You know your cat would be into proper English.
+#Add a method and concatenate your string so that is has proper capitalization and punctuation.
+print "i can't believe how dumb this dog is"
+
+#Your translator keeps using the words "will not" instead of "will"
+#Add a method to the end of this string to sub out one word for the other.
+print "I will not take over the world, you mark my words. Also I will not eat the dogs food when he isn't looking."
+
+#There's a lot going on here. Your translator is really having trouble.
+#Fix each sentence as you see fit - proper grammar please!
+#This time, the dog is yelling. Your cat never yells.
+print "uoy dluoc woh ,syawyna doof ym tae ot gniog si eh ,em erofeb tac eht gnideef ruoy eveileb tnac I"
+
+print "you are THE VERY BEST HUMAN, do not give in to his whining, he is a not stupid dog."
